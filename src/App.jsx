a6104b64f567f8892e8ceb282fcf242a9f7e0ff4
dhwanisarokar <dhwanisarokar@gmail.com>
2024-03-28 14:52:22 +0530

    impliment stopwatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Stopwatch from "./components/Stopwatch";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
+      <div className="stopwatch">
         <h1>Stopwatch</h1>
+        <Stopwatch />
       </div>
     </>
   );
---
+++ src/components/Stopwatch.jsx
@@ -0,0 +1,49 @@
+import { useRef } from "react";
+import { useState } from "react";
+
+export default function Stopwatch() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [time, setTime] = useState(0);
+
+  const timeIntervalId = useRef(null);
+
+  const startStopwatch = () => {
+    if (isRunning) {
+      clearInterval(timeIntervalId.current);
+    } else {
+      clearInterval(timeIntervalId.current);
+      timeIntervalId.current = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    }
+    setIsRunning(!isRunning);
+  };
+
+  const resetStopwatch = () => {
+    clearInterval(timeIntervalId.current);
+    setTime(0);
+    setIsRunning(false);
+  };
+
+  const formatTime = (time) => {
+    console.log(time);
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+
+    return `${minutes.toString().padStart(2, "0")}:${seconds
+      .toString()
+      .padStart(2, "0")}`;
+  };
+
+  return (
+    <div>
+      <p style={{ textAlign: "center" }}>Time: {formatTime(time)}</p>
+      <button className="startBtn" onClick={startStopwatch}>
+        {isRunning ? "Stop" : "Start"}
+      </button>
+      <button className="resetBtn" onClick={resetStopwatch}>
+        Reset
+      </button>
+    </div>
+  );
+}
